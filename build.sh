82b52742d8dc85b85f103aeaeb8c1a6e789977af
SDUBZ <swh654587@gmail.com>
2019-09-30 11:53:58 +1000

    tweaks
    fixed and launching

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+gradle wrapper --gradle-version $(gradle --version | grep Gradle | awk '{print $2}') --distribution-type all
 ./gradlew clean build
 read -p "Press any key to continue..."
